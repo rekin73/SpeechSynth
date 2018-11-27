37598f7a46db1648e429ff76643c1ebe35681780
rekin73 <runefor99@gmail.com>
2018-11-27 14:24:05 +0100

    lol

--- server/server.js
+++ server/server.js
@@ -2,8 +2,17 @@ const express = require('express')
 const app = express()
 const port = 3000
 const fs=require('fs');
+const xpath=require('xpath'),dom = require('xmldom').DOMParser
+const path=require('path');
 app.get('/', (req, res) => {
-    res.send('asdf')
+    
+    //let file=fs.open("kurs.xml");
+    var xml = fs.open(path.join(__dirname, '/', 'kurs.xml'))
+    var doc = new dom().parseFromString(xml)
+    var nodes = xpath.select("//title", doc)
+    console.log(nodes[0].localName + ": " + nodes[0].firstChild.data)
+    console.log("Node: " + nodes[0].toString())
+    res.sendFile(path.join(__dirname, '/', 'kurs.xml'))
 })
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
