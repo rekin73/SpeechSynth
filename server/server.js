2ef57f7915562a83743d681cd5b816714f477023
rekin73 <runefor99@gmail.com>
2018-12-04 02:21:46 +0100

    Prace nad serwerem

--- server/server.js
+++ server/server.js
@@ -4,15 +4,32 @@ const port = 3000
 const fs=require('fs');
 const xpath=require('xpath'),dom = require('xmldom').DOMParser
 const path=require('path');
+const xml = require('xml2js');
+const parser=new xml.Parser();
 app.get('/', (req, res) => {
     
     //let file=fs.open("kurs.xml");
-    var xml = fs.open(path.join(__dirname, '/', 'kurs.xml'))
-    var doc = new dom().parseFromString(xml)
-    var nodes = xpath.select("//title", doc)
-    console.log(nodes[0].localName + ": " + nodes[0].firstChild.data)
-    console.log("Node: " + nodes[0].toString())
-    res.sendFile(path.join(__dirname, '/', 'kurs.xml'))
+    fs.readFile(path.join(__dirname, '/', 'kurs.xml'),"utf-8",function(error,text){
+if(error){
+    console.log(error);
+}else{
+    //var doc = new dom().parseFromString(text)
+    parser.parseString(text,function(err,result){
+        if(error){
+            console.log(err);
+        }
+        else{
+        var nodes = result['kurs']['lekcja']
+        res.send(nodes)
+        }
+    });
+    
+    
+}
+    })
+    
+
+    //res.sendFile(path.join(__dirname, '/', 'kurs.xml'))
 })
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
