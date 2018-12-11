aa61d308ddf3c671b6dffb99894f54eeaee0fa75
rekin73 <runefor99@gmail.com>
2018-12-11 01:00:59 +0100

    working

--- KalFiszki/index.php
+++ KalFiszki/index.php
@@ -1,9 +1,9 @@
 <?php
-header("Access-Control-Allow-Origin: *");
+header('Access-Control-Allow-Origin: *');
 $nr;
 
 $xml=simplexml_load_file('kurs.xml');
 $xml=$xml->xpath('//lekcja');
-print_r($xml[0]['slowo']);
+//print_r($xml[0][1]);
 echo json_encode($xml[0]);
 ?>
